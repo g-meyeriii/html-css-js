01d4359c71b3a190b247f15eb6ebc9343af537e6
max-student <max-student@maxtrain.com>
2020-03-12 21:21:07 -0400

    calculators

--- JS/index.js
+++ JS/index.js
@@ -10,5 +10,33 @@ const clickme3 = () => {
     let val = ctrl.value;
     let lblCtrl = document.getElementById("lbl");
     lblCtrl.innerHTML = "<h3>"+val+"</h3>";  //text block space above and below
+
+}
+const plus = () => {
+    let v1 = document.getElementById("num1");
+    let v2 = document.getElementById("num2");
+    let val1 = v1.value;
+    let val2 = v2.value;
+    let totalCtrl = document.getElementById("total");
+    totalCtrl.value = +val1+ Number(val2);
     
 }
+const add = ()=> {
+    let v1 = document.getElementById("num1").value;
+    sum += +val
+    display();
+    
+}
+const add = ()=> {
+    let v1 = document.getElementById("num1").value;
+    sum -= +val
+    display();
+}
+const clr =() => {
+    document.getElementById("num1").value = 0;
+    document.getElementById("total2").value = 0;
+    display();
+}
+
+
+    
